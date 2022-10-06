425693d19b5815ec7f7c837d783f7880a3921840
GlennBecker <nnelgxorz@gmail.com>
2022-10-05 20:02:15 -0600

    Seams toggle and some css animation

--- host/src/routes/layout.tsx
+++ host/src/routes/layout.tsx
@@ -2,18 +2,19 @@ import {
 	component$,
 	Slot,
 	useContextProvider,
-	useStore,
+	useStore
 } from '@builder.io/qwik';
 import { AppState, GlobalAppState } from './constants';
 
 export default component$(() => {
 	const store = useStore<AppState>({
-		showSeams: false,
+		showSeams: true,
 	});
 	useContextProvider(GlobalAppState, store);
 	return (
 		<main data-seams={store.showSeams}>
 			<Slot />
+			<button onClick$={() => store.showSeams = !store.showSeams}>Show Seams</button>
 		</main>
 	);
 });
