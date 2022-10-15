44ff3d37df93bd2292e835cbe40407f8f857c22b
GlennBecker <nnelgxorz@gmail.com>
2022-10-15 08:12:02 -0600

    Improve types, add ports for MFEs

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -1,12 +1,12 @@
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
-import { defineConfig } from 'vite';
+import { defineConfig, ServerOptions } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { remotes } from '../../libs/shared/remotes';
 
-let proxy = {};
+let proxy: ServerOptions["proxy"] = {};
 Object.values(remotes).forEach(({ name, url }) => {
-	proxy[`^/${name}/.*`] = {
+	proxy![`^/${name}/.*`] = {
 		target: url.replace(`${name}/`, ''),
 	};
 });
--- libs/shared/cloudflare.utils.ts
+++ libs/shared/cloudflare.utils.ts
@@ -1,6 +1,6 @@
-import { remotes } from './remotes';
+import { type RemoteData } from './remotes';
 
-export const getTargetUrl = (request, remote: typeof remotes.menu) => {
+export const getTargetUrl = (request, remote: RemoteData) => {
 	return (
 		remote.url.replace(`${remote.name}/`, '') +
 		request.url.split(`/${remote.name}/`)[1]
