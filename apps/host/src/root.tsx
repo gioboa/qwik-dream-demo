0c1ae12f7c582fc3c94a1b3e35ef16650c52401b
gioboa <giorgiob.boa@gmail.com>
2022-12-08 00:59:57 +0100

    update host

--- apps/host/src/root.tsx
+++ apps/host/src/root.tsx
@@ -1,25 +1,16 @@
 import { component$, useStyles$ } from '@builder.io/qwik';
-import {
-	QwikCity,
-	RouterOutlet,
-	ServiceWorkerRegister,
-} from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
+import { QwikCityProvider, RouterOutlet } from '@builder.io/qwik-city';
 
-import styles from './global.css?inline';
+import globalStyles from './global.css?inline';
 
 export default component$(() => {
-	useStyles$(styles);
+	useStyles$(globalStyles);
 	return (
-		<QwikCity>
-			<head>
-				<meta charSet='utf-8' />
-				<RouterHead />
-			</head>
-			<body lang='en'>
+		<QwikCityProvider>
+			<div />
+			<body lang="en">
 				<RouterOutlet />
-				<ServiceWorkerRegister />
 			</body>
-		</QwikCity>
+		</QwikCityProvider>
 	);
 });
