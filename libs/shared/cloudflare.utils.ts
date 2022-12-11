5433cb0f08af64d5fe4adcef85651eac2ca1ccde
gioboa <giorgiob.boa@gmail.com>
2022-12-11 21:57:22 +0100

    change cloudflare getTargetUrl

--- libs/shared/cloudflare.utils.ts
+++ libs/shared/cloudflare.utils.ts
@@ -1,8 +1,5 @@
 import { type RemoteData } from './remotes';
 
 export const getTargetUrl = (request, remote: RemoteData) => {
-	return (
-		remote.url.replace(`${remote.name}/`, '') +
-		request.url.split(`/${remote.name}/`)[1]
-	);
+	return remote.url + request.url.split(`/${remote.name}/`)[1];
 };
