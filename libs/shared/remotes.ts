66cfff7cc99e106f84bc2776ba74a3543830c1be
gioboa <giorgiob.boa@gmail.com>
2022-12-11 20:39:16 +0100

    set localhost remotes

--- libs/shared/remotes.ts
+++ libs/shared/remotes.ts
@@ -11,32 +11,32 @@ export interface RemoteData {
 export const remotes: Record<string, RemoteData> = {
 	menu: {
 		name: 'menu',
-		url: 'https://qwik-dream-menu.pages.dev/menu/',
+		url: 'http://localhost:5003/menu/',
 		secondsOfDelay: 0,
 		seamsColor: '#007d81',
 	},
 	hero: {
 		name: 'hero',
-		url: 'https://qwik-dream-hero.pages.dev/hero/',
+		url: 'http://localhost:5002/hero/',
 		secondsOfDelay: 0,
 		seamsColor: '#800006',
 		defaultQueryParam: 'qwik',
 	},
 	product: {
 		name: 'product',
-		url: 'https://qwik-dream-product.pages.dev/product/',
+		url: 'http://localhost:5004/product/',
 		secondsOfDelay: 0,
 		seamsColor: '#0031ff',
 	},
 	reviews: {
 		name: 'reviews',
-		url: 'https://qwik-dream-reviews.pages.dev/reviews/',
+		url: 'http://localhost:5005/reviews/',
 		secondsOfDelay: 0,
 		seamsColor: '#bc0000',
 	},
 	cart: {
 		name: 'cart',
-		url: 'https://qwik-dream-cart.pages.dev/cart/',
+		url: 'http://localhost:5006/cart/',
 		secondsOfDelay: 0,
 		seamsColor: '',
 		extraStyles: { 'z-index': 10 },
