f5acdd70828e5e17e98c0f51adca19603f09104b
gioboa <giorgiob.boa@gmail.com>
2022-12-12 15:23:43 +0100

    add CustomEvent types in shared file
    Close #15

--- libs/shared/custom-events.ts
+++ libs/shared/custom-events.ts
@@ -1,3 +1,12 @@
-export const orderChangeEventId = 'orderChangeEvent';
-export const cartQuantitiesChangedEventId = 'cartQuantitiesChangedEvent';
-export const sessionTokenReceivedEventId = 'sessionTokenReceivedEvent';
+export const SESSION_TOKEN_KEY = 'vendure-session-token';
+export const ORDER_CHANGE_EVENT = 'ORDER_CHANGE_EVENT';
+export const CART_QUANTITIES_CHANGED_EVENT = 'CART_QUANTITIES_CHANGED_EVENT';
+export const SESSION_TOKEN_RECEIVED_EVENT = 'SESSION_TOKEN_RECEIVED_EVENT';
+
+export const cartQuantitiesChangedEvent = (productVariantQuantities: Record<number, number>) =>
+	new CustomEvent(CART_QUANTITIES_CHANGED_EVENT, { detail: productVariantQuantities });
+
+export const sessionTokenReceivedEvent = (sessionToken: string) =>
+	new CustomEvent(SESSION_TOKEN_RECEIVED_EVENT, { detail: { sessionToken } });
+
+export const orderChangeEvent = () => new CustomEvent(ORDER_CHANGE_EVENT);
