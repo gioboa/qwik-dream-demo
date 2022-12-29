8b5fbd3993e10bd00053e07e5c6e4c4873c3c2e5
gioboa <giorgiob.boa@gmail.com>
2022-12-29 11:55:53 +0100

    add unobserve method

--- apps/host/src/components/component-loader/ComponentLoader.tsx
+++ apps/host/src/components/component-loader/ComponentLoader.tsx
@@ -9,26 +9,27 @@ export default component$<Props>(({ remote }) => {
 	const elementRef = useSignal<Element>();
 
 	useClientEffect$(() => {
-			const observer = new IntersectionObserver(
-				async ([element]) => {
-					if (element.isIntersecting) {
-						const response = await fetch(`/${remote.name}/`);
-						if (response.ok) {
-							const rawHtml = await response.text();
-							elementRef.value!.innerHTML = rawHtml;
-							observer.disconnect();
-						}
+		const observer = new IntersectionObserver(
+			async ([element]) => {
+				if (element.isIntersecting) {
+					const response = await fetch(`/${remote.name}/`);
+					if (response.ok) {
+						const rawHtml = await response.text();
+						elementRef.value!.innerHTML = rawHtml;
+						observer.disconnect();
 					}
-				},
-				{ rootMargin: '150px' }
-			)
-			observer.observe(elementRef.value!)
-	})
+				}
+			},
+			{ rootMargin: '150px' },
+		);
+		observer.observe(elementRef.value!);
+		return () => {
+			observer.unobserve(elementRef.value!);
+		};
+	});
 
 	return (
-		<div
-			class="remote-component"
-		>
+		<div class="remote-component">
 			<p class="remote-label">{remote.url}</p>
 			<div ref={elementRef}>loading...</div>
 		</div>
