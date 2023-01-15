9950d61ec3ba36ddfb918f0c4d28e40eeb043d7d
dario-piotrowicz <dario.piotrowicz@gmail.com>
2023-01-15 21:05:46 +0000

    remove ComponentLoader in favour of RemoteMfe
    Remove the ComponentLoader component used to fetch the reviews fragment
    and use the RemoteMfe component for that instead.
    
    This removes code duplication (we were duplicating the seams wrapper)
    and fixed the reviews fragment not being fetched during development.
    
    resolves #36

--- apps/host/src/components/remote-mfe/remote-mfe.tsx
+++ apps/host/src/components/remote-mfe/remote-mfe.tsx
@@ -1,54 +1,115 @@
-import { component$, SSRStream, SSRStreamBlock } from '@builder.io/qwik';
+import {
+	component$,
+	SSRStream,
+	SSRStreamBlock,
+	StreamWriter,
+	useClientEffect$,
+	useSignal,
+} from '@builder.io/qwik';
 import { useLocation } from '@builder.io/qwik-city';
 import type { RemoteData } from '../../../../../libs/shared/remotes';
 
 export interface Props {
 	remote: RemoteData;
+	fetchOnScroll?: true;
 }
 
-export default component$(({ remote }: Props) => {
-	const localtion = useLocation();
+export default component$(({ remote, fetchOnScroll }: Props) => {
+	const location = useLocation();
 	const { seamsColor, extraStyles, hideLabel } = remote;
-	const url = `${remote.url}${localtion.query[remote.name] || remote.defaultQueryParam || ''}`;
-	const decoder = new TextDecoder();
+	const url = `${remote.url}${location.query[remote.name] || remote.defaultQueryParam || ''}`;
+
+	const scrollElementRef = useFetchOnScroll(!!fetchOnScroll, url);
+
 	return (
-		<SSRStreamBlock>
-			<div class="remote-component" style={{ '--seams-color': seamsColor, ...(extraStyles ?? {}) }}>
-				{!hideLabel && (
-					<a target="blank" href={url} class="remote-label">
-						{url}
-					</a>
-				)}
-				<SSRStream>
-					{async stream => {
-						const fragment = await fetch(url, { headers: { accept: 'text/html' } });
-						const reader = fragment.body!.getReader();
-						let fragmentChunk = await reader.read();
-						let base: string = '';
-						while (!fragmentChunk.done) {
-							let rawHtml = decoder.decode(fragmentChunk.value);
-							if (import.meta.env.DEV) {
-								// TODO: These regexes are a hack to work around the fact that the streamed content is failing to prefix base path.
-								rawHtml = rawHtml.replace(/q:base="\/(\w+)\/build\/"/gm, (match, child) => {
-									base = '/' + child;
-									console.log('FOUND', base);
-									return match;
-								});
-								rawHtml = rawHtml.replace(/="(\/src\/([^"]+))"/gm, (match, path) => {
-									console.log('REPLACE', path);
-									return '="' + base + path + '"';
-								});
-								rawHtml = rawHtml.replace(/"\\u0002(\/src\/([^"]+))"/gm, (match, path) => {
-									console.log('REPLACE', path);
-									return '"\\u0002' + base + path + '"';
-								});
-							}
-							stream.write(rawHtml);
-							fragmentChunk = await reader.read();
-						}
-					}}
-				</SSRStream>
-			</div>
-		</SSRStreamBlock>
+		<div class="remote-component" style={{ '--seams-color': seamsColor, ...(extraStyles ?? {}) }}>
+			{!hideLabel && (
+				<a target="blank" href={url} class="remote-label">
+					{url}
+				</a>
+			)}
+			{fetchOnScroll ? (
+				<div ref={scrollElementRef}>loading...</div>
+			) : (
+				<SSRStreamBlock>
+					<SSRStream>{getSSRStreamFunction(url)}</SSRStream>
+				</SSRStreamBlock>
+			)}
+		</div>
 	);
 });
+
+export function useFetchOnScroll(enabled: boolean, url: string) {
+	const scrollElementRef = useSignal<Element>();
+
+	useClientEffect$(({ track }) => {
+		track(() => scrollElementRef.value);
+
+		if (scrollElementRef.value && enabled) {
+			const observer = new IntersectionObserver(
+				async ([element]) => {
+					if (element.isIntersecting) {
+						const response = await fetchRemote(url);
+						if (response.ok) {
+							const rawHtml = await response.text();
+							const { html } = fixRemoteHTMLInDevMode(rawHtml);
+							scrollElementRef.value!.innerHTML = html;
+							observer.disconnect();
+						}
+					}
+				},
+				{ rootMargin: '150px' },
+			);
+			observer.observe(scrollElementRef.value!);
+			return () => {
+				observer.unobserve(scrollElementRef.value!);
+			};
+		}
+	});
+
+	return scrollElementRef;
+}
+
+export function fetchRemote(url: string): Promise<Response> {
+	return fetch(url, { headers: { accept: 'text/html' } });
+}
+
+export function getSSRStreamFunction(remoteUrl: string) {
+	const decoder = new TextDecoder();
+
+	return async (stream: StreamWriter) => {
+		const reader = (await fetchRemote(remoteUrl)).body!.getReader();
+		let fragmentChunk = await reader.read();
+		let base = '';
+		while (!fragmentChunk.done) {
+			const rawHtml = decoder.decode(fragmentChunk.value);
+			const fixedHtmlObj = fixRemoteHTMLInDevMode(rawHtml, base);
+			base = fixedHtmlObj.base;
+			stream.write(fixedHtmlObj.html);
+			fragmentChunk = await reader.read();
+		}
+	};
+}
+
+/**
+ * This function is a hack to work around the fact that in dev mode the remote html is failing to prefix the base path.
+ */
+export function fixRemoteHTMLInDevMode(rawHtml: string, base = ''): { html: string; base: string } {
+	let html = rawHtml;
+	if (import.meta.env.DEV) {
+		html = html.replace(/q:base="\/(\w+)\/build\/"/gm, (match, child) => {
+			base = '/' + child;
+			console.log('FOUND', base);
+			return match;
+		});
+		html = html.replace(/="(\/src\/([^"]+))"/gm, (match, path) => {
+			console.log('REPLACE', path);
+			return '="' + base + path + '"';
+		});
+		html = html.replace(/"\\u0002(\/src\/([^"]+))"/gm, (match, path) => {
+			console.log('REPLACE', path);
+			return '"\\u0002' + base + path + '"';
+		});
+	}
+	return { html, base };
+}
