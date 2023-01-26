c82d5dd7a3b6cd7f31d3fb4f78986d3619207ca5
Dmitriy Stepanenko <stepanenkodmitri@gmail.com>
2023-01-26 19:56:03 +0200

    refactor: cloudflare pages config

--- apps/host/functions/[[path]].ts
+++ apps/host/functions/[[path]].ts
@@ -1,5 +1,6 @@
+// eslint-disable-next-line
 // @ts-ignore
 
 // Cloudflare Pages Functions
 // https://developers.cloudflare.com/pages/platform/functions/
-export { onRequest } from '../server/entry.cloudflare-pages';
+export { onRequest } from '../../../dist/apps/host/server/entry.cloudflare-pages';
