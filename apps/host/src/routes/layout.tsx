6faa93615c574e2ef2804b5ca0a5110c955e329d
gioboa <giorgiob.boa@gmail.com>
2023-11-08 22:41:48 +0100

    feat: set cookie

--- apps/host/src/routes/layout.tsx
+++ apps/host/src/routes/layout.tsx
@@ -1,5 +1,11 @@
 import { component$, Slot, useContextProvider, useStore } from '@builder.io/qwik';
 import { AppState, GlobalAppState } from '../store';
+import { RequestHandler } from '@builder.io/qwik-city';
+import { setCookie } from '../utils/cookie';
+
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+	cacheControl({ staleWhileRevalidate: 60 * 60 * 24 * 7, maxAge: 5 });
+};
 
 export default component$(() => {
 	const store = useStore<AppState>({
@@ -8,12 +14,26 @@ export default component$(() => {
 	useContextProvider(GlobalAppState, store);
 	return (
 		<div data-seams={store.showSeams}>
-			<button
-				class="flex m-2 p-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700"
-				onClick$={() => (store.showSeams = !store.showSeams)}
-			>
-				Show Worker URLs
-			</button>
+			<div class="flex gap-3">
+				<button
+					class="flex mt-3 ml-3 p-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700"
+					onClick$={() => (store.showSeams = !store.showSeams)}
+				>
+					Show Worker URLs
+				</button>
+				<button
+					class="flex mt-3 p-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700"
+					onClick$={() => setCookie('USER', 'Giorgio')}
+				>
+					User Giorgio
+				</button>
+				<button
+					class="flex mt-3 p-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700"
+					onClick$={() => setCookie('USER', 'Miško')}
+				>
+					User Miško
+				</button>
+			</div>
 			<Slot />
 		</div>
 	);
