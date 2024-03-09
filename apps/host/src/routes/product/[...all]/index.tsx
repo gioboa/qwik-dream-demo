9cc5c69682d4af1c835c0cb6b75d798b156c6872
gioboa <giorgiob.boa@gmail.com>
2024-03-09 16:06:15 +0100

    feat: add Proxys

--- apps/host/src/routes/product/[...all]/index.tsx
+++ apps/host/src/routes/product/[...all]/index.tsx
@@ -1,9 +1,21 @@
 import { RequestHandler } from '@builder.io/qwik-city';
 import { remotes } from '@qwikdream/shared';
 
-export const onRequest: RequestHandler = async ({ redirect, url }) => {
+export const onRequest: RequestHandler = async ({ send, url }) => {
 	const remoteName = 'product';
 	const pathName = url.pathname.replace(`/${remoteName}/`, '');
-	const remoteUrl = pathName ? remotes[remoteName].url.replace(`${remoteName}/`, '') : remotes[remoteName].url;
-	throw redirect(308, remoteUrl + pathName + url.search);
+	const remoteUrl = pathName
+		? remotes[remoteName].url.replace(`${remoteName}/`, '')
+		: remotes[remoteName].url;
+
+	const response = await fetch(remoteUrl + pathName + url.search);
+	const content = await response.text();
+	send(
+		new Response(content, {
+			status: 200,
+			headers: {
+				'Content-Type': response.headers.get('Content-Type') || 'application/javascript',
+			},
+		}),
+	);
 };
--- apps/host/src/routes/reviews/[...all]/index.tsx
+++ apps/host/src/routes/reviews/[...all]/index.tsx
@@ -1,12 +1,21 @@
 import { RequestHandler } from '@builder.io/qwik-city';
 import { remotes } from '@qwikdream/shared';
 
-export const onRequest: RequestHandler = async ({ text, url }) => {
+export const onRequest: RequestHandler = async ({ send, url }) => {
 	const remoteName = 'reviews';
 	const pathName = url.pathname.replace(`/${remoteName}/`, '');
 	const remoteUrl = pathName
 		? remotes[remoteName].url.replace(`${remoteName}/`, '')
 		: remotes[remoteName].url;
+
 	const response = await fetch(remoteUrl + pathName + url.search);
-	text(200, await response.text());
+	const content = await response.text();
+	send(
+		new Response(content, {
+			status: 200,
+			headers: {
+				'Content-Type': response.headers.get('Content-Type') || 'application/javascript',
+			},
+		}),
+	);
 };
