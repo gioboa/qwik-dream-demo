510e39a1fa2343ff0aa5dfa4f0f2302ad566f3ad
gioboa <giorgiob.boa@gmail.com>
2024-03-29 22:47:05 +0100

    fix: add hard coded reviews
    that endpoint is no longer available

--- apps/reviews/src/routes/index.tsx
+++ apps/reviews/src/routes/index.tsx
@@ -5,9 +5,44 @@ import StarIcon from '../components/icons/StarIcon';
 import { Review } from '../types';
 
 export const useReviewsData = routeLoader$(async () => {
-	const endPoint = 'https://mocki.io/v1/9eeeec1a-4494-428d-81da-0db85e6b5b37';
-	const response = await fetch(endPoint);
-	return (await response.json()) as Review[];
+	const reviews = [
+		{
+			id: 1,
+			title: 'I love it!',
+			rating: 5,
+			content: '\n' +
+				'              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\n' +
+				'            ',
+			author: 'Miško H',
+			date: 'May 25, 2022',
+			datetime: '2022-05-25'
+		},
+		{
+			id: 2,
+			title: 'Awesome product',
+			rating: 5,
+			content: '\n' +
+				'              <p>Ornare quam viverra orci sagittis eu volutpat odio. Massa id neque aliquam vestibulum morbi blandit cursus risus at. Ultrices tincidunt arcu non sodales neque.</p> \n' +
+				'              <p>Mattis pellentesque id nibh tortor id aliquet lectus proin nibh. Pellentesque diam volutpat commodo sed egestas egestas fringilla. Sodales ut etiam sit amet nisl purus in mollis nunc. Turpis egestas integer eget aliquet nibh praesent tristique magna. Augue interdum velit euismod in pellentesque massa placerat duis ultricies. Justo laoreet sit amet cursus sit amet.</p>\n' +
+				'            ',
+			author: 'Adam B',
+			date: 'May 24, 2022',
+			datetime: '2022-05-24'
+		},
+		{
+			id: 3,
+			title: 'Really happy with this purchase',
+			rating: 5,
+			content: '\n' +
+				'              <p>Nisi est sit amet facilisis magna etiam tempor orci eu.</p> \n' +
+				'              <p>Elit duis tristique sollicitudin nibh sit amet commodo. Dolor sit amet consectetur adipiscing elit. Lorem dolor sed viverra ipsum nunc. Accumsan tortor posuere ac ut consequat semper. Augue mauris augue neque gravida in fermentum et sollicitudin ac.</p>\n' +
+				'            ',
+			author: 'Steve S',
+			date: 'May 24, 2022',
+			datetime: '2022-05-24'
+		}
+	];
+	return reviews as Review[];
 });
 
 export default component$(() => {
